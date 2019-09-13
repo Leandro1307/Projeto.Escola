285146eeab91b15a4eb67e04f3abb75a8c50c370
a <a>
2019-09-12 21:56:52 -0300

    Creating Aluno

---
+++ Escola/src/escola/CriarAluno.java
@@ -0,0 +1,15 @@
+package escola;
+
+public class CriarAluno {
+
+	public static void main(String[] args) {
+		// Nesta classe, vamos de fato criar um aluno.
+		Aluno aluno1 = new Aluno();
+		aluno1.nome = "Juliana";
+		aluno1.idade = 34;
+		aluno1.nota1 = 7.5;
+		aluno1.nota2 = 8.2;
+
+	}
+
+}
